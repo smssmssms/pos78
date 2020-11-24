f6b2a822b9dc9599fb6fcf48b9001e225d81501f
smssmssms <smssmssms@users.noreply.github.com>
2020-11-24 16:13:04 +0000

    Update config.h

--- config.h
+++ config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID   0x0000
 #define DEVICE_VER   0x0001
 #define MANUFACTURER smssmssms
-#define PRODUCT      POS78
+#define PRODUCT      pos78
 
 /* key matrix size */
 #define MATRIX_ROWS 6
