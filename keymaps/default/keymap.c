f7be84e2dc25fb0d0abb9f5b51ed192fd5f3f6af
smssmssms <smssmssms@users.noreply.github.com>
2020-11-24 13:08:29 +0000

    Update keymap.c

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -22,10 +22,6 @@ enum layer_names {
     _FN
 };
 
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-    //none
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
